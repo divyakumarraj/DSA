ef65236fcb079eb348c2eab07669ec73956fc66b
Divya Prakash Gupta <77239774+divyakumarraj@users.noreply.github.com>
2022-01-24 18:12:10 +0530

    Create Scratch.java

---
+++ Scratch.java
@@ -0,0 +1,18 @@
+//removing character from word
+public class Scratch {
+    public static String remove(String str){
+        if(str.length()==0){
+            return str;
+    }
+        String result="";
+        if(str.charAt(0)!='a'){
+            result=result+str.charAt(0);
+    }
+        String recans=remove(str.substring(1));
+        return result + recans;
+    }
+    public static void main(String[] args) {
+        String name="divya";
+        System.out.println(remove(name));
+    }
+    }
