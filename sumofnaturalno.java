976be1ddb4f12fc22c205114c8376c52cad4a98f
Divya Prakash Gupta <77239774+divyakumarraj@users.noreply.github.com>
2022-03-10 22:18:48 +0530

    Create sumofnaturalno.java

---
+++ sumofnaturalno.java
@@ -0,0 +1,14 @@
+public class sumofnaturalno {
+    public static int sum(int n){
+        if(n==1){
+            return 1;
+
+        }
+        return sum(n-1)+n;
+    }
+
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(sum(n));
+    }
+}
