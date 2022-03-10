1ecf9199b1439e132e0d4822a7855916aee3cd2c
Divya Prakash Gupta <77239774+divyakumarraj@users.noreply.github.com>
2022-03-10 22:19:54 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,16 @@
+public class factorial {
+    static int fact(int n){
+        if(n==0)
+        {
+            return 1;
+        }
+        int smallAns= fact(n-1);
+        return n * smallAns;
+    }
+
+    public static void main(String[] args) {
+        int n= 3;
+        int ans = fact(n);
+        System.out.println(ans);
+    }
+}
